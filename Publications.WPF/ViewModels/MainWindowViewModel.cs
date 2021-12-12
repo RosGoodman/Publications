185791c813b669adf52a01af83960fec3147ab60
Pavel <pavel.official@bk.ru>
2021-12-13 00:01:37 +0300

    проверка привязки wpf

---
+++ Publications.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Publications.WPF.ViewModels.Base;
+
+namespace Publications.WPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Главное окно программы.";
+
+        public string Title { get => _title; set => _title = value; }
+    }
+}
