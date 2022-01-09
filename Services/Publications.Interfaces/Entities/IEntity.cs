bc778422933cbde008cecb851e558199e75dd6bd
Pavel <pavel.official@bk.ru>
2022-01-09 11:14:40 +0300

    Интерфейс репозитория

---
+++ Services/Publications.Interfaces/Entities/IEntity.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Publications.Interfaces.Entities;
+
+public interface IEntity
+{
+    int Id { get; set; }
+}
---
+++ Services/Publications.Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using Publications.Interfaces.Entities;
+
+namespace Publications.Interfaces.Repositories;
+
+public interface IRepository<T> where T : class, IEntity
+{
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken cancel = default);
+
+    async Task<T?> GetAsync(int id, CancellationToken cancel = default)
+    {
+        var all = await GetAllAsync(cancel).ConfigureAwait(false);
+        return all.FirstOrDefault(item => item.Id == id);
+    }
+
+    Task<int> AddAsync(T entity, CancellationToken cancel = default);
+
+    Task<bool> UpdateAsync(T entity, CancellationToken cancel = default);
+
+    Task<bool> DeleteAsync(T entity, CancellationToken cancel = default);
+}
